ee530fed06a49414030f292b9fb75938ded9f971
raphaeIl <michaelsliu3@gmail.com>
2024-03-31 00:54:30 -0400

    implement basic text recognition with Tesseract

---
+++ MomoiAssist/ViewModels/ImageRecognizer.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Tesseract;
+
+// Install: "Install-Package Tesseract" using Package Manager Console with Nuget
+namespace MomoiAssist.ViewModels {
+
+    public class ImageRecognizer {
+
+        private readonly TesseractEngine engine;
+
+        public ImageRecognizer() { 
+            this.engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.LstmOnly);
+            this.engine.SetVariable("tessedit_char_whitelist", "0123456789:.");
+        }
+
+        public async Task<string> RecognizeTextAsync() {
+            return await Task.Run(RecognizeText);
+        }
+
+        public string RecognizeText() {
+            byte[] screenshot = ImageRecognizer.GetScreenShot(1640, 34, 160, 45);
+            Pix image = Pix.LoadFromMemory(screenshot);
+
+            var page = this.engine.Process(image);
+            var text = page.GetText().Trim();
+            page.Dispose();
+
+            return text;
+        }
+
+        public static byte[] GetScreenShot(int x, int y, int width, int height) {
+            Bitmap bitmap = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.CopyFromScreen(x, y, 0, 0, new System.Drawing.Size(width, height));
+            
+            MemoryStream memoryStream = new MemoryStream();
+            bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+
+            return memoryStream.ToArray();
+        }
+
+        public static void SaveImage(byte[] imageBytes, string filePath, System.Drawing.Imaging.ImageFormat format) {
+            MemoryStream ms = new MemoryStream(imageBytes);
+            Image image = Image.FromStream(ms);
+            
+            image.Save(filePath, format);
+            Trace.WriteLine("Image saved");
+        }
+
+        public static bool IsValidDurationFormat(string text) {
+            string pattern = @"^\d{2}:\d{2}\.\d{3}$";
+            return Regex.IsMatch(text, pattern);
+        }
+    }
+
+}
--- MomoiAssist/ViewModels/Pages/DashboardViewModel.cs
+++ MomoiAssist/ViewModels/Pages/DashboardViewModel.cs
@@ -1,14 +1,30 @@
-﻿namespace MomoiAssist.ViewModels.Pages
-{
-    public partial class DashboardViewModel : ObservableObject
-    {
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace MomoiAssist.ViewModels.Pages {
+    public partial class DashboardViewModel : ObservableObject {
+
         [ObservableProperty]
-        private int _counter = 0;
+        private string _duration = "Duration";
 
         [RelayCommand]
-        private void OnCounterIncrement()
-        {
-            Counter++;
+        private void OnCounterIncrement() {
+            Task.Run(UpdateDurationDisplay);
+        }
+
+        public async void UpdateDurationDisplay() {
+            ImageRecognizer imageRecognizer = new ImageRecognizer();
+
+            while (true) {
+                String text = await imageRecognizer.RecognizeTextAsync();
+
+                if (ImageRecognizer.IsValidDurationFormat(text))
+                    Duration = text;
+
+                //Trace.WriteLine(text);
+            }
+
         }
     }
 }
+
